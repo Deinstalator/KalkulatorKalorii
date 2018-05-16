22bb021368d7f089f4d596059f82dc23b55e03df
Deinstalator <radoslaw.antoniak@gmail.com>
2018-05-16 23:50:10 +0200

    Dodano products.component

--- KalkulatorKalorii/ClientApp/app/components/products/components/products.component.ts
+++ KalkulatorKalorii/ClientApp/app/components/products/components/products.component.ts
@@ -1,20 +1,40 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../models/product';
 import { ProductsService } from '../services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './products.component.html'
 })
 
 export class ProductsComponent implements OnInit {
-    constructor(private productsService: ProductsService) { };
+    constructor(
+        private productsService: ProductsService,
+        private router: Router
+    ) { };
+
+    products: Array<Product> = new Array<Product>();
+    pateTitle: string = "Lista dostępnych produktów";
+    tempInfo: string = "Loading...";
 
     testowaZmienna = "Pozdrowienia z komponentu";
 
     ngOnInit(): void {
-        this.productsService.getProducts().subscribe(
-            props => { console.log("Products", props) },
-            error => { console.log("Error", error) }
+        this.downloadProducts();
         );
+    }
+
+    downloadProducts(): void {
+        this.productsService.getProducts().subscribe(
+            productsFromDb => {
+                if (productsFromDb.length == 0) {
+                    this.tempInfo = "Records not found";
+                }
+                else {
+                    this.products = productsFromDb;
+                }
+            },
+            error => console.log(error)
+        )
     }
 }
